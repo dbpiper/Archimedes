b7876515b72af55d12b5f83392f40bca4248e110
David Piper <david.piper@mailfence.com>
2019-05-06 16:59:43 -0500

    feat(client): create H1 atom for front-end based on UI mockup
    I am going to follow the atomic design layout for structuring the
    front end as well as the design, as I really like it and think that
    it is a pretty good approach. It automatically enforces the
    components to be as simple as possible, thus making the React
    front-end code well-structured and easy to navigate.
    
    The previous directory structure was way too rigid and made things
    incredibly confusing, since it was based on screen and digging down
    tons of layers. I think that atomic design will have many of the
    same benefits, without many of the problems and will also have
    the benefits of a "flat" directory structure. Thus, bringing the
    best of both worlds!
    
    Signed-off-by: David Piper <david.piper@mailfence.com>

---
+++ src/client/src/atoms/H1.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  font-family: 'Varela Round', sans-serif;
+  font-size: 24px;
+  color: #000000;
+  /* filter: drop-shadow(0, 4px, 4px, #000000); */
+
+  filter: drop-shadow(0 4px 3px #adadad);
+`;
+
+export default H1;
---
+++ src/client/src/atoms/__stories__/H1.stories.tsx
@@ -0,0 +1,5 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import H1 from '../H1';
+
+storiesOf('atoms/H1', module).addWithJSX('H1', () => <H1>H1</H1>);
